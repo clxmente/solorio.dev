0ff009b369d5262c6725cc8b7d2d1411180fe843
Clemente Solorio <solorioeclem164@gmail.com>
2022-09-11 00:38:55 -0700

    wip: pushing progress

---
+++ components/navbar.tsx
@@ -0,0 +1,10 @@
+const Nav = () => {
+  return (
+    <nav className="w-full bg-white/10 backdrop-blur-sm px-4 py-3 flex space-x-4 rounded-md">
+      <div className="text-white">item1</div>
+      <div className="text-white">item1</div>
+    </nav>
+  );
+};
+
+export default Nav;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+
+import Nav from "../components/navbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="mx-auto max-w-4xl py-10 px-5">
+      <Nav />
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
