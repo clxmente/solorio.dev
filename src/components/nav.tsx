83ef3348f6e8bb20eb5e6f777a77e4e1d12447eb
clxmente <solorioeclem164@gmail.com>
2022-09-11 15:52:08 -0700

    somewhat transition on the mobile nav

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { AnimatePresence, motion } from "framer-motion";
-import { Disclosure } from "@headlessui/react";
+import { Disclosure, Transition } from "@headlessui/react";
 
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 
@@ -9,7 +9,7 @@ const Nav = () => {
     <motion.div
       initial={{ y: -10, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
-      transition={{ duration: 0.3 }}
+      transition={{ duration: 0.5 }}
       className="sticky top-4 z-50"
     >
       <Disclosure
@@ -19,54 +19,70 @@ const Nav = () => {
         {/* Open indicates whether the disclosure is open or not */}
         {({ open }) => (
           <>
-            <div className="">
-              {/* Mobile Open/Close Btn */}
-              <Disclosure.Button className="rounded-xl p-2 text-white hover:text-gray-300 sm:hidden">
-                {!open ? (
-                  <Bars3Icon className="h-6 w-6" />
-                ) : (
-                  <XMarkIcon className="h-6 w-6" />
-                )}
-              </Disclosure.Button>
-              {/* Mobile Open/Close Btn */}
+            {/* Mobile Open/Close Btn */}
+            <Disclosure.Button
+              className="rounded-xl p-2 text-white hover:text-gray-300 sm:hidden"
+              onClick={() =>
+                void new Audio("/pop.mp3").play().catch(() => null)
+              }
+            >
+              {!open ? (
+                <Bars3Icon className="h-6 w-6" />
+              ) : (
+                <XMarkIcon className="h-6 w-6" />
+              )}
+            </Disclosure.Button>
+            {/* Mobile Open/Close Btn */}
 
-              {/* Desktop Links */}
-              <div className="hidden space-x-4 sm:flex">
-                <Link href="/">
-                  <a className="rounded-md p-2 font-semibold text-gray-300 hover:bg-gray-700/30">
-                    Home
-                  </a>
-                </Link>
-                <Link href="/contact">
-                  <a className="rounded-md p-2 font-semibold text-gray-300 hover:bg-gray-700/30">
-                    Contact
-                  </a>
-                </Link>
-              </div>
-              {/* Desktop Links */}
+            {/* Desktop Links */}
+            <div className="hidden space-x-4 sm:flex">
+              <Link href="/">
+                <a className="rounded-md p-2 font-semibold text-gray-300 hover:bg-gray-700/30">
+                  Home
+                </a>
+              </Link>
+              <Link href="/contact">
+                <a className="rounded-md p-2 font-semibold text-gray-300 hover:bg-gray-700/30">
+                  Contact
+                </a>
+              </Link>
             </div>
+            {/* Desktop Links */}
 
             {/* Mobile Links */}
-            <Disclosure.Panel className="my-2 border-t border-gray-700 sm:hidden">
-              <div className="mt-2 flex flex-col space-y-1">
-                <Link href="/" passHref>
-                  <Disclosure.Button
-                    as="a"
-                    className="rounded-md p-2 font-semibold text-gray-300 hover:bg-gray-700/30"
-                  >
-                    Home
-                  </Disclosure.Button>
-                </Link>
-                <Link href="/contact" passHref>
-                  <Disclosure.Button
-                    as="a"
-                    className="rounded-md p-2 font-semibold text-gray-300 hover:bg-gray-700/30"
-                  >
-                    Contact
-                  </Disclosure.Button>
-                </Link>
-              </div>
-            </Disclosure.Panel>
+            <Transition
+              show={open}
+              enter="transition duration-500 ease-out"
+              enterFrom="transform scale-95 opacity-0"
+              enterTo="transform scale-100 opacity-100"
+              leave="transition duration-250 ease-out"
+              leaveFrom="transform scale-100 opacity-100"
+              leaveTo="transform scale-95 opacity-0"
+            >
+              <Disclosure.Panel
+                className="my-2 border-t border-gray-700 sm:hidden"
+                static
+              >
+                <div className="mt-2 flex flex-col space-y-1">
+                  <Link href="/" passHref>
+                    <Disclosure.Button
+                      as="a"
+                      className="rounded-md p-2 font-semibold text-gray-300 hover:bg-gray-900/50"
+                    >
+                      Home
+                    </Disclosure.Button>
+                  </Link>
+                  <Link href="/contact" passHref>
+                    <Disclosure.Button
+                      as="a"
+                      className="rounded-md p-2 font-semibold text-gray-300 hover:bg-gray-900/50"
+                    >
+                      Contact
+                    </Disclosure.Button>
+                  </Link>
+                </div>
+              </Disclosure.Panel>
+            </Transition>
             {/* Mobile Links */}
           </>
         )}
