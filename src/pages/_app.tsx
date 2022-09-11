0b5cd413378892555357ae944511de32bedd15f6
clxmente <solorioeclem164@gmail.com>
2022-09-11 14:34:33 -0700

    thanks alii (and phin)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,8 +5,15 @@ import { AnimatePresence, motion } from "framer-motion";
 
 import Nav from "../components/navbar";
 import Head from "next/head";
+import { useEffect } from "react";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps, router }: AppProps) {
+  useEffect(() => {
+    // thanks alii (and phineas for the pop sound)
+    if (typeof window === "undefined") return;
+    void new Audio("/pop.mp3").play().catch(() => null);
+  }, [router.pathname]);
+
   return (
     <div className="mx-auto max-w-4xl space-y-8 py-10 px-5 sm:px-0">
       <Head>
