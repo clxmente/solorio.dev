f85770f6af12c83b36592e7a03a68025edd9d91b
clxmente <solorioeclem164@gmail.com>
2022-09-13 10:04:21 -0700

    redirects

--- next.config.js
+++ next.config.js
@@ -1,4 +1,38 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/github",
+        destination: "https://github.com/clxmente",
+        permanent: true,
+      },
+      {
+        source: "/linkedin",
+        destination: "https://www.linkedin.com/in/clementesolorio/",
+        permanent: true,
+      },
+      {
+        source: "/twitter",
+        destination: "https://twitter.com/clxmente",
+        permanent: true,
+      },
+      {
+        source: "/instagram",
+        destination: "https://www.instagram.com/clxmente/",
+        permanent: true,
+      },
+      {
+        source: "/discord",
+        destination: "https://discord.com/users/393069508027351051",
+        permanent: true,
+      },
+      {
+        source: "/spotify",
+        destination: "https://open.spotify.com/user/udije5cy07hhruod1t8b9lb7b",
+        permanent: true,
+      },
+    ];
+  },
+};
